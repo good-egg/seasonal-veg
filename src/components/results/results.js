992d5ab12d91f9e5f496d0ea4c8515188f77d0fb
Becky Rush <becky.rush@bbc.co.uk>
2021-03-07 11:05:56 +0000

    fix is/are grammar for singleproduce ending in S (eg asparagus)

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -79,8 +79,9 @@ export default class Results {
     }
 
     isOrAreCheck(string) {
+        const nonPluralWordsEndingInS = ['asparagus', 'watercress']
         const isOneWord = string.includes(' ') ? false : true;
-        const endsInS = (str) => str.charAt(str.length - 1) === 's' ? 'are' : 'is';
+        const endsInS = (str) => str.charAt(str.length - 1) === 's' && !nonPluralWordsEndingInS.includes(str.toLowerCase()) ? 'are' : 'is';
         return (!isOneWord && string.includes('(')) ? endsInS(string.split(' ')[0]) : endsInS(string);
     }
     
